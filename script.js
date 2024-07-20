7ed57221d730546066918a359d4fe4c720143b7b
Ankita Sinha <156523529+AnkitaSinha23@users.noreply.github.com>
2024-07-20 16:40:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+console.log("Welcome to Spotify");
+
+let audioElement = new Audio('Gracie_Abrams_-_Where_Do_We_Go_Now_.mp3');
+let songIndex = 0;
+let masterPlay = document.getElementById('masterplay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+let songs = [
+    {songName: "Where do we go now", filePath: 'Gracie_Abrams_-_Where_Do_We_Go_Now_.mp3', coverPath: 'cover.jpg'},
+    {songName: "Best", filePath: 'best.mp3', coverPath: 'cover2.jpg'},
+    {songName: "Fault Line", filePath: 'fault line.mp3', coverPath: 'cover3.jpg'},
+    {songName: "Will you cry", filePath: 'will you cry.mp3', coverPath: 'cover4.jpg'},
+];
+
+// Initialize song items
+songItems.forEach((element, i) => {
+    element.getElementsByTagName("img")[0].src = 'cover.jpg';
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+});
+
+// Handle play/pause click
+masterPlay.addEventListener('click', () => {
+    if (audioElement.paused || audioElement.currentTime <= 0) {
+        audioElement.play();
+        masterPlay.classList.remove('fa-play');
+        masterPlay.classList.add('fa-pause');
+        gif.style.opacity = 1;
+    } else {
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause');
+        masterPlay.classList.add('fa-play');
+        gif.style.opacity = 0;
+    }
+});
+
+// Listen to events
+audioElement.addEventListener('timeupdate', () => {
+    let progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+    myProgressBar.value = progress;
+});
+
+myProgressBar.addEventListener('change', () => {
+    audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
+});
+
+const makeAllPlays = () => {
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    });
+};
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element, index) => {
+    element.addEventListener('click', (e) => {
+        makeAllPlays();
+        songIndex = index;
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = songs[songIndex].filePath;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play');
+        masterPlay.classList.add('fa-pause');
+    });
+});
+
+document.getElementById('next').addEventListener('click', () => {
+    if (songIndex >= songs.length - 1) {
+        songIndex = 0;
+    } else {
+        songIndex += 1;
+    }
+    audioElement.src = songs[songIndex].filePath;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play');
+    masterPlay.classList.add('fa-pause');
+});
+
+document.getElementById('previous').addEventListener('click', () => {
+    if (songIndex <= 0) {
+        songIndex = 0;
+    } else {
+        songIndex -= 1;
+    }
+    audioElement.src = songs[songIndex].filePath;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play');
+    masterPlay.classList.add('fa-pause');
+});
